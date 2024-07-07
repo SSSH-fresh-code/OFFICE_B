83403c840e453734ee68e2784e4dd995f3c5f07d
limcpf <daeseong0226@gmail.com>
2024-07-07 21:21:42 +0900

    feat: topic entity 및 관련 dto 생성

---
+++ src/domain/blog/domain/topic/topic.interface.ts
@@ -0,0 +1,37 @@
+import { AggregateRoot } from "src/domain/aggregate-root.interface";
+import { ReadTopicDto } from "../../presentation/topic/dto/read-topic.dto";
+
+export interface iTopic {
+
+  /**
+   * 토피 id를 반환합니다.
+   */
+  get id(): number;
+
+  /**
+   * 토피 이름을 반환합니다.
+   */
+  get name(): string;
+
+  /**
+   * topic name을 설정합니다.
+   * @param {string} name
+   */
+  set name(name: string);
+
+  /**
+   * 생성일을 반환합니다.
+   */
+  get createdAt(): Date;
+
+  /**
+   * 수정일을 반환합니다.
+   */
+  get updatedAt(): Date;
+
+
+  /**
+   * dto로 변환합니다.
+   */
+  toDto(): ReadTopicDto;
+}
---
+++ src/domain/blog/presentation/topic/dto/read-topic.dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, IsString } from "class-validator";
+
+export class ReadTopicDto {
+  @ApiProperty({
+    description: '토픽 id',
+    example: 1,
+  })
+  id: number;
+
+  @ApiProperty({
+    description: '토픽 이름',
+    example: "sample",
+  })
+  @IsString()
+  name: string;
+
+
+  @ApiProperty({
+    description: '생성 일시',
+    example: new Date(),
+  })
+  @IsDate()
+  createdAt: Date;
+
+  @ApiProperty({
+    description: '수정 일시',
+    example: new Date(),
+  })
+  @IsDate()
+  updatedAt: Date;
+}
