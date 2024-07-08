ab62c74add72c4df2e4f781643227ae161159b55
limcpf <daeseong0226@gmail.com>
2024-07-08 22:21:40 +0900

    feat: repository.ts 작성
    - prisma-topic.repository.ts 작성
    - prisma-topic.repository.spec.ts 우선 작성함

--- src/infrastructure/db/repositories/prisma-topic.repository.spec.ts
+++ src/infrastructure/db/repositories/prisma-topic.repository.spec.ts
@@ -3,9 +3,9 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { LoggerModule } from "src/infrastructure/module/logger.module";
 import { PrismaService } from "../prisma.service";
 import { TopicRepository } from "src/domain/blog/infrastructure/topic/topic.repository";
-import { PrismatopicRepository } from "./prisma-topic.repository";
+import { PrismaTopicRepository } from "./prisma-topic.repository";
 import { Topic } from "src/domain/blog/domain/topic/topic.entity";
-import { equal } from "assert";
+import { Prisma } from "@prisma/client";
 
 describe('PrismaTopicRepository', () => {
   let repository: TopicRepository;
@@ -18,12 +18,12 @@ describe('PrismaTopicRepository', () => {
       imports: [ConfigModule.forRoot({ isGlobal: true }), LoggerModule],
       providers: [
         PrismaService,
-        PrismatopicRepository,
+        PrismaTopicRepository,
         ConfigService
       ],
     }).compile();
 
-    repository = module.get<TopicRepository>(PrismatopicRepository);
+    repository = module.get<TopicRepository>(PrismaTopicRepository);
     prisma = module.get<PrismaService>(PrismaService);
   });
 
@@ -37,52 +37,117 @@ describe('PrismaTopicRepository', () => {
 
   describe('findById', () => {
     it('id로 Topic을 조회합니다.', async () => {
-      const t = await prisma.topic.create({ data: dummyTopic });
+      const t = await prisma.topic.create({
+        data: {
+          id: dummyTopic.id,
+          name: dummyTopic.name
+        }
+      });
 
       const topic = await repository.findById(t.id);
 
       expect(t.id).toEqual(topic.id);
       expect(t.name).toEqual(topic.name);
     });
-    it('존재하지 않는 Topic을 조회합니다.', async () => {
 
+    it('존재하지 않는 Topic을 조회합니다.', async () => {
+      await expect(() => repository.findById(1)).rejects.toThrow(Prisma.PrismaClientKnownRequestError)
     });
   });
   describe('findByName', () => {
     it('name으로 Topic을 조회합니다.', async () => {
+      const t = await prisma.topic.create({
+        data: {
+          id: dummyTopic.id,
+          name: dummyTopic.name
+        }
+      });
 
+      const topic = await repository.findByName(t.name);
+
+      expect(t.id).toEqual(topic.id);
+      expect(t.name).toEqual(topic.name);
     });
+
     it('존재하지 않는 Topic을 조회합니다.', async () => {
-
+      await expect(() => repository.findByName("Wrong")).rejects.toThrow(Prisma.PrismaClientKnownRequestError)
     });
-
   });
   describe('save', () => {
     it('Topic을 생성합니다.', async () => {
+      const topic = await repository.save(dummyTopic);
 
+      expect(dummyTopic.id).not.toEqual(topic.id);
+      expect(dummyTopic.name).toEqual(topic.name);
     });
-    it('이미 존재하는 이름으로 Topic을 생성합니다.', async () => {
 
+    it('이미 존재하는 이름으로 Topic을 생성합니다.', async () => {
+      await prisma.topic.create({
+        data: {
+          id: dummyTopic.id,
+          name: dummyTopic.name
+        }
+      });
+
+      await expect(() => repository.save(dummyTopic)).rejects.toThrow(Prisma.PrismaClientKnownRequestError)
     });
   });
   describe('update', () => {
     it('Topic을 수정합니다.', async () => {
+      const updateName = dummyTopic.name + "2";
+      const { id } = await prisma.topic.create({
+        data: {
+          id: dummyTopic.id,
+          name: dummyTopic.name
+        }
+      });
 
+      const t = new Topic(id, updateName);
+
+      const topic = await repository.update(t);
+
+      expect(id).toEqual(topic.id);
+      expect(dummyTopic.name).not.toEqual(topic.name);
+      expect(updateName).toEqual(topic.name);
     });
+
     it('이미 존재하는 이름으로 Topic을 수정합니다.', async () => {
+      await prisma.topic.create({
+        data: {
+          id: dummyTopic.id,
+          name: dummyTopic.name
+        }
+      });
 
+      const { id } = await prisma.topic.create({
+        data: {
+          name: dummyTopic.name + "2"
+        }
+      });
+
+      const t = new Topic(id, dummyTopic.name);
+
+      await expect(() => repository.update(t)).rejects.toThrow(Prisma.PrismaClientKnownRequestError)
     });
+
     it('존재하지 않는 Topic을 수정합니다.', async () => {
-
+      await expect(() => repository.update(dummyTopic)).rejects.toThrow(Prisma.PrismaClientKnownRequestError)
     });
-
   });
+
   describe('delete', () => {
     it('Topic을 삭제합니다.', async () => {
+      const { name } = await prisma.topic.create({
+        data: {
+          name: dummyTopic.name
+        }
+      });
 
+      expect(() => repository.delete(name)).resolves;
     });
-    it('존재하지 않는 Topic을 삭제합니다.', async () => {
 
+    it('존재하지 않는 Topic을 삭제합니다.', async () => {
+      await expect(() => repository.delete(dummyTopic.name)).rejects.toThrow(Prisma.PrismaClientKnownRequestError)
     });
   });
 })
--- src/infrastructure/db/repositories/prisma-topic.repository.ts
+++ src/infrastructure/db/repositories/prisma-topic.repository.ts
@@ -1,23 +1,51 @@
 import { TopicRepository } from "src/domain/blog/infrastructure/topic/topic.repository";
 import { PrismaService } from "../prisma.service";
 import { Topic } from "src/domain/blog/domain/topic/topic.entity";
+import { Injectable } from "@nestjs/common";
 
-export class PrismatopicRepository implements TopicRepository {
+@Injectable()
+export class PrismaTopicRepository implements TopicRepository {
   constructor(private readonly prisma: PrismaService) { }
 
+
   async findById(id: number): Promise<Topic> {
-    throw new Error("method not implement");
+    const topic = await this.prisma.topic.findUniqueOrThrow({
+      where: { id }
+    });
+
+    return Topic.of(topic);
   }
+
   async findByName(name: string): Promise<Topic> {
-    throw new Error("method not implement");
+    const topic = await this.prisma.topic.findUniqueOrThrow({
+      where: { name }
+    });
+
+    return Topic.of(topic);
   }
+
   async save(topic: Topic): Promise<Topic> {
-    throw new Error("method not implement");
+    const entity = await this.prisma.topic.create({
+      data: { name: topic.name }
+    })
+
+    return Topic.of(entity);
   }
+
   async update(topic: Topic): Promise<Topic> {
-    throw new Error("method not implement");
+    const entity = await this.prisma.topic.update({
+      where: { id: topic.id },
+      data: {
+        name: topic.name
+      }
+    });
+
+    return Topic.of(entity);
   }
+
   async delete(name: string): Promise<void> {
-    throw new Error("method not implement");
+    await this.prisma.topic.delete({
+      where: { name }
+    });
   }
 }
